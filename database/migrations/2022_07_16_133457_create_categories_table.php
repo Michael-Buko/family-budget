c0951121c5e1532049edd4a61b57e2b555443789
Michael <5610280@gmail.com>
2022-09-11 18:43:12 +0300

    Add Models and Migrations

---
+++ database/migrations/2022_07_16_133457_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable(false)->constrained('users');
+            $table->string('title')->nullable(false);
+            $table->foreignId('type_id')->nullable(false)->constrained('types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
